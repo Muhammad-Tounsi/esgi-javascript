3c283e41580930cd7c84fa1996f4fc9598ac331b
muhammad-tounsi <muhammad.tounsi@hotmail.fr>
2019-01-01 16:25:14 +0100

    Fix exercice 1 and 2

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,5 +1,28 @@
 function type_check_v1(value, type){
-	return typeof value === type;
+	if(typeof value === type || value instanceof Object || value instanceof Array){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 console.log(type_check_v1(1, "number"));
+
+function type_check_v2(valeur, confType){
+	if(confType.hasOwnProperty('type'){
+		if(confType.hasOwnProperty('value'){
+			return confType.value == valeur;
+		}
+		else if(type_check_v1(valeur, confType)){
+			return true;
+		}
+	//return false;
+	}
+	else if(confType.hasOwnProperty('enum')){
+		if(confType.enum.includes(valeur)){
+			return true;
+		}
+	}
+}
+
